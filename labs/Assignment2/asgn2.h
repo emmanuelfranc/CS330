320f7a16f9dedb1cb3f93234af6955aa8840593b
Emmanuel Francis <109141026+emmanuelfranc@users.noreply.github.com>
2024-01-26 20:50:11 +0000

    Assignment on track

--- labs/Assignment2/asgn2.h
+++ labs/Assignment2/asgn2.h
@@ -14,7 +14,7 @@ bool isPrime(int n)
 {
     if (n <= 1)
         return false;
-    for (int i = 2; i <= n / 2; i++)
+    for (int i = 2; i <= n / 2; i++) // i starts from 2 up to n/2, it keeps dividing n
     {
         if (n % i == 0)
         {
@@ -29,8 +29,8 @@ bool isPowOfTwo(int n)
     if (n <= 0)
         return false;
 
-    for (int i = n; i > 1; i /= 2)
-    {
+    for (int i = n; i > 1; i /= 2) //As long as i is greater than 1, keep dividing i until i mod 2 is no longer 0.
+    {                              //if i arrives at a point wjere it's not a modulus of 2, then i is not a power of 2.
         if (i % 2 != 0)
             return false;
     }
@@ -50,7 +50,7 @@ void cubeOfOdd(int n)
 
 void introToCS330(int n)
 {
-    if (n % 3 == 0 && n % 7 == 0)
+    if (n % 3 == 0 && n % 7 == 0) // first check if n is divisible by both 3 and 7 as checking 7 or 3 first will return an unexpected output
     {
         printf("UAB CS 330\n");
     }
@@ -62,13 +62,13 @@ void introToCS330(int n)
     {
         printf("CS\n");
     }
-    else if (isPrime(n) && n != 3 && n != 7)
+    else if (isPrime(n) && n != 3 && n != 7) // call the isPrime function to determine whether or not n is a prime number
     {
         printf("Go Blazers\n");
     }
     else
     {
-        printf("%d\n", n * n * n);
+        printf("%d\n", n * n * n); // print n cube
     }
 }
 
@@ -76,7 +76,7 @@ void printHello(int n)
 {
     for (int i = 0; i <= n; i++)
     {
-        if (isPowOfTwo(i))
+        if (isPowOfTwo(i)) // call the isPowOfTwo function to determine whether or not n is a power of 2
         {
             printf("HELLO");
         }
@@ -97,7 +97,7 @@ int paintGallons(float length, float width, float height)
 
     float totalArea = 2 * (area1 + area2) + areaCeil;
 
-    return ceil(totalArea / 400);
+    return ceil(totalArea / 400); //round up the result from the calculation
 }
 
 void grader(float avg_exams, float avg_hw, int attendance)
