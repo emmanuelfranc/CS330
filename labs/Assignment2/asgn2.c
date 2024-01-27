d179600aefec0f2a7029c571b4f2f4ac9fcbde84
Emmanuel Francis <109141026+emmanuelfranc@users.noreply.github.com>
2024-01-27 15:57:48 +0000

    Assignment 2 completed.

--- labs/Assignment2/asgn2.c
+++ labs/Assignment2/asgn2.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include "asgn2.h"
 
+//The main method
 int main()
 {
     int n;
     printf("Please enter an integer value: ");
     scanf("%d", &n);
     printf("CubeOfOdd -:\n");
-    cubeOfOdd(n);
+    cubeOfOdd(n); //cubeOfOdd function call 
 
     printf("\nintroToCS330 -:\n");
-    introToCS330(n);
+    introToCS330(n); //function call
 
     printf("\nprintHello -:\n");
-    printHello(n);
+    printHello(n); //function call
 
-    printf("\npaintGallons -:\n");
-    int gallons = paintGallons(10, 12, 8);
+
+    float i,j,k;
+    printf("\nPlease enter three float values separated by spaces: ");
+    scanf("%f %f %f", &i, &j, &k);
+    printf("paintGallons -:\n");
+    int gallons = paintGallons(i, j, k); //function call
     printf("%d\n", gallons);
 
-    printf("\ngrader -:\n");
-    grader(72, 88, 22);
+
+    float l, m;
+    int p;
+    printf("\nPlease enter two floats and one integer separated by spaces in that order: ");
+    scanf("%f %f %d", &l, &m, &p);
+
+    printf("grader -:\n");
+    grader(l, m, p); //function call
 }
--- labs/Assignment2/asgn2.h
+++ labs/Assignment2/asgn2.h
@@ -1,6 +1,7 @@
 #ifndef __asgn2__
 #define __asgn2__
 
+#include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
 
@@ -10,33 +11,43 @@ we haven't already included this header*/
 /* your functions go here */
 // Note: main() goes in the asgn2.c file
 
+
+/** function isPrime takes an integer argument n and returns true if the number is a prime number
+ *  returns false if it is not
+*/
 bool isPrime(int n)
 {
-    if (n <= 1)
+    if (n <= 1)         //If number is less than or equal to 1, then it is not prime.
         return false;
-    for (int i = 2; i <= n / 2; i++) // i starts from 2 up to n/2, it keeps dividing n
-    {
-        if (n % i == 0)
+    for (int i = 2; i <= n / 2; i++) // i starts from 2 up to n/2, 
+    {                                
+        if (n % i == 0)             // If n is divisibe by any number between 2 and n/2, it is not prime.
         {
             return false;
         }
     }
-    return true;
+    return true;                    //return true if no divisors are found.
 }
 
+/* function isPowOfTwo, just like isPrime returns a boolean (true or false). 
+** If the integer argument - n is a power of 2, return true. Else, return fasle.
+*/
 bool isPowOfTwo(int n)
 {
     if (n <= 0)
         return false;
 
-    for (int i = n; i > 1; i /= 2) //As long as i is greater than 1, keep dividing i until i mod 2 is no longer 0.
-    {                              //if i arrives at a point wjere it's not a modulus of 2, then i is not a power of 2.
+    for (int i = n; i > 1; i /= 2) //As long as i is greater than 1, keep looping, dividing by 2 in each step
+    {                              //if i arrives at a point where it's not divisible by 2, then i is not a power of 2.
         if (i % 2 != 0)
             return false;
     }
     return true;
 }
 
+/* function cubeOfOdd takes an integer parameter -n and prints the cube of all odd numbers less than n.
+** returns void. 
+*/
 void cubeOfOdd(int n)
 {
     for (int i = 1; i < n; i++) // i starts from 1 and goes to n - 1
@@ -48,6 +59,9 @@ void cubeOfOdd(int n)
     }
 }
 
+/** function takes an integer parameter -n, returns void and prints to the output
+ *  depending on different conditions
+*/
 void introToCS330(int n)
 {
     if (n % 3 == 0 && n % 7 == 0) // first check if n is divisible by both 3 and 7 as checking 7 or 3 first will return an unexpected output
@@ -72,6 +86,10 @@ void introToCS330(int n)
     }
 }
 
+/**
+ * PrintHello takes an integer -n and prints a string of integers from 0 to n
+ * It prints HELLO for every power of 2.
+*/
 void printHello(int n)
 {
     for (int i = 0; i <= n; i++)
@@ -88,6 +106,11 @@ void printHello(int n)
     printf("\n");
 }
 
+/**
+ * printGallons takes 3 parameters of type float which represent a rectangular room.
+ * It commputers the amount of paint needed to cover four walls and celings.
+ * Returns an integer which is the value of calculations rounded up to the nearest whole number.
+*/
 int paintGallons(float length, float width, float height)
 {
     float area1 = length * height;
@@ -100,6 +123,10 @@ int paintGallons(float length, float width, float height)
     return ceil(totalArea / 400); //round up the result from the calculation
 }
 
+/**
+ * grader takes two floats and one integer, returns void and prints pass or fail depending on different criteria.
+ * function uses the calculation done with the parameters to determine whether to print pass or fail.
+*/
 void grader(float avg_exams, float avg_hw, int attendance)
 {
     if ((attendance > 20) && (avg_exams > 70) && (avg_hw > 70) && (avg_exams > 85 || avg_hw > 85))
