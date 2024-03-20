a7ed0374d965ea2c68fb6ce5315cb5aae4a3796d
Emmanuel Francis <109141026+emmanuelfranc@users.noreply.github.com>
2024-03-20 00:27:44 +0000

    First assembly attempts

---
+++ labs/Assignment05/asgn5.s
@@ -0,0 +1,61 @@
+	.file	"asgn5.c"
+	.text
+	.globl	myAddTwoNumbersFunction
+	.type	myAddTwoNumbersFunction, @function
+myAddTwoNumbersFunction:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	myAddTwoNumbersFunction, .-myAddTwoNumbersFunction
+	.section	.rodata
+.LC0:
+	.string	"The answer is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$10, -12(%rbp)
+	movl	$7, -8(%rbp)
+	movl	$0, -4(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-12(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	myAddTwoNumbersFunction
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
