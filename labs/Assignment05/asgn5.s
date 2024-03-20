46dd7c69b00cbc7e9e65f607c1141df9e3fcc5ee
Emmanuel Francis <109141026+emmanuelfranc@users.noreply.github.com>
2024-03-20 02:01:29 +0000

    Assignment underway

--- labs/Assignment05/asgn5.s
+++ labs/Assignment05/asgn5.s
@@ -1,20 +1,20 @@
-	.file	"asgn5.c"
-	.text
-	.globl	myAddTwoNumbersFunction
-	.type	myAddTwoNumbersFunction, @function
-myAddTwoNumbersFunction:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	movl	-4(%rbp), %edx
-	movl	-8(%rbp), %eax
-	addl	%edx, %eax
+	.file	"asgn5.c"										# Indicates the source file name
+	.text													# Start of the text (code) section
+	.globl	myAddTwoNumbersFunction							# Makes myAddTwoNumbersFunction globally accessible
+	.type	myAddTwoNumbersFunction, @function				# Declares myAddTwoNumbersFunction as a function type
+myAddTwoNumbersFunction:									# Label marking the start of myAddTwoNumbersFunction
+.LFB0:														# Indicates the begining of the function's local block
+	.cfi_startproc											# Signals the start of the procedure			
+	pushq	%rbp											# Pushes the base pointer on ot the stack
+	.cfi_def_cfa_offset 16									# Adjusts the stack pointer
+	.cfi_offset 6, -16										# Defines the offset for %rbp in the Call Frame Information
+	movq	%rsp, %rbp										# Sets the base pointer to the current stack pointer
+	.cfi_def_cfa_register 6									# Defines %rbp as the current frame pointer
+	movl	%edi, -4(%rbp)									# Moves the first argument (in %edi) to a memory location on the stack
+	movl	%esi, -8(%rbp)									# Moves the second argument (in %esi) to a memory location on the stack
+	movl	-4(%rbp), %edx									# Moves the first argument from the stack to the refister %edx
+	movl	-8(%rbp), %eax									# Moves the second argument frmo the stack to register %eax
+	addl	%edx, %eax										# Adds the value of %edx and %eax and stores the result in %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
